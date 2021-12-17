879c6da6a6ccf329b84fade82347b97932a45012
mixvlad <kozlovmv@pik.ru>
2021-12-17 23:44:12 +0300

    d2 second part

--- d2_dive/main.go
+++ d2_dive/main.go
@@ -41,4 +41,40 @@ func main() {
 	}
 
 	fmt.Println(x * y)
+	second_part()
+}
+
+func second_part() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	x, y, aim := 0, 0, 0
+	for scanner.Scan() {
+		words := strings.Fields(scanner.Text())
+		if len(words) > 0 {
+			intVar, _ := strconv.Atoi(words[1])
+			direction := words[0]
+			if direction == "forward" {
+				x += intVar
+				y += intVar * aim
+			}
+			if direction == "down" {
+				aim += intVar
+			}
+			if direction == "up" {
+				aim -= intVar
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(x * y)
 }
