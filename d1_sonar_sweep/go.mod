0a90c560a8e9bf7a05e0a04edcb89f7dfc2fd66f
mixvlad <kozlovmv@pik.ru>
2021-12-17 17:19:41 +0300

    Fix structure

---
+++ d1_sonar_sweep/go.mod
@@ -0,0 +1,3 @@
+module d1_sonar_sweep
+
+go 1.17
--- d1_sonar_sweep/main.go
+++ d1_sonar_sweep/main.go
@@ -1,7 +1,30 @@
-package d1_sonar_sweep
+package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
-func AllUsers() {
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Run() {
 	fmt.Println("All Users")
 }
