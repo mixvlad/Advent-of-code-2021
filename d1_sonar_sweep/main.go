de8d5e96a4923728bd73e4ab567667e72e4ce85e
mixvlad <kozlovmv@pik.ru>
2021-12-17 23:22:04 +0300

    d1 second part

--- d1_sonar_sweep/main.go
+++ d1_sonar_sweep/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"log"
 	"os"
@@ -25,10 +26,38 @@ func main() {
 			count++
 		}
 		intPointer = &intVar
-		// if prevX == nil {
+	}
 
-		// }
-		//fmt.Println(scanner.Text())
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(count)
+	three_measurement()
+}
+
+func three_measurement() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	// optionally, resize scanner's capacity for lines over 64K, see next example
+	count := 0
+	queue := list.New()
+	for scanner.Scan() {
+		intVar, _ := strconv.Atoi(scanner.Text())
+		queue.PushFront(intVar)
+		e := queue.Back()
+		if queue.Len() > 3 {
+			prevVar, _ := e.Value.(int)
+			if intVar > prevVar {
+				count++
+			}
+			queue.Remove(e)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
