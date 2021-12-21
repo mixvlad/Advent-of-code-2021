305dde33e34f305e8938a2799aee7601328d1861
mixvlad <kozlovmv@pik.ru>
2021-12-21 12:13:47 +0300

    d9 second part

--- d9_smoke_basin/main.go
+++ d9_smoke_basin/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -58,18 +59,59 @@ func main() {
 	}
 
 	height := 0
+	lenBasin := []int{}
 	for i := range heights {
 		for j := range heights[i] {
 			if isLowest(i, j, heights) {
 				height += heights[i][j] + 1
+
+				basin := []pair{{i, j}}
+				basin = findBasin(heights, basin, i, j)
+				lenBasin = append(lenBasin, len(basin))
 			}
 		}
 	}
 
 	fmt.Println(height)
-	secondPart()
+	sort.Slice(lenBasin, func(i, j int) bool {
+		return lenBasin[j] < lenBasin[i]
+	})
+	fmt.Println(lenBasin[0] * lenBasin[1] * lenBasin[2])
 }
 
-func secondPart() {
+func Contains(what pair, where []pair) bool {
+	for _, x := range where {
+		if what == x {
+			return true
+		}
+	}
+	return false
+}
 
+func Chech(heights [][]int, basin []pair, i, j int) []pair {
+	if heights[i][j] < 9 && !Contains(pair{i, j}, basin) {
+		basin = append(basin, pair{i, j})
+		basin = findBasin(heights, basin, i, j)
+	}
+	return basin
+}
+
+func findBasin(heights [][]int, basin []pair, i, j int) []pair {
+	if i > 0 {
+		basin = Chech(heights, basin, i-1, j)
+	}
+	if j > 0 {
+		basin = Chech(heights, basin, i, j-1)
+	}
+	if i < len(heights)-1 {
+		basin = Chech(heights, basin, i+1, j)
+	}
+	if j < len(heights[i])-1 {
+		basin = Chech(heights, basin, i, j+1)
+	}
+	return basin
+}
+
+type pair struct {
+	x, y int
 }
